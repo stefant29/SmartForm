6750ba851c65e92f8feb24149199383b7410df06
Stefan Toncu <stefan.toncu29@gmail.com>
2016-10-12 17:41:44 +0600

    TAB press: remove logs

--- SmartForm/src/utils/MyKeyListener.java
+++ SmartForm/src/utils/MyKeyListener.java
@@ -9,8 +9,9 @@ import javax.swing.JTextField;
 public class MyKeyListener implements KeyListener {
 	Autocomplete autoComplete = null;
 	JTextField textField = null;
-	
-	public MyKeyListener() {}
+
+	public MyKeyListener() {
+	}
 
 	public MyKeyListener(Autocomplete autoComplete, JTextField textField) {
 		this.autoComplete = autoComplete;
@@ -21,47 +22,29 @@ public class MyKeyListener implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 		boolean knownLetter = true;
 
-		System.out.println(autoComplete.keywords);
 		String prefix = String.valueOf(e.getKeyChar());
-		System.out.println("input: " + prefix);
 		int n = Collections.binarySearch(autoComplete.keywords, prefix);
 		if (n < 0 && -n <= autoComplete.keywords.size()) {
 			String match = autoComplete.keywords.get(-n - 1);
-			System.out.println("match: " + match);
-			System.out.println("StartsWith: " + match.startsWith(prefix));
-			if (match.startsWith(prefix)) {
-				System.out.println("GOOD");
+			if (match.startsWith(prefix))
 				knownLetter = true;
-			} else {
-				System.out.println("BAD");
+			else
 				knownLetter = false;
-			}
-		} else {
+		} else
 			knownLetter = false;
-			System.out.println("Not Found");
-		}
 
-		if (e.getKeyCode() != 9 && e.getKeyCode() != 8 && e.getKeyCode() != 32 && knownLetter) {
-			System.out.println("set to false");
+		if (e.getKeyCode() != 9 && e.getKeyCode() != 8 && e.getKeyCode() != 32 && knownLetter)
 			textField.setFocusTraversalKeysEnabled(false);
-		} else {
-			System.out.println("set to true");
+		else
 			textField.setFocusTraversalKeysEnabled(true);
-		}
-
-		System.out.println();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
